3437e626f4c854b620264e55f6f657220eb66720
geo <geo3005@hotmail.com>
2017-02-04 14:09:18 +0200

    read xml from file or url

--- get-exch-rates/main.go
+++ get-exch-rates/main.go
@@ -1,30 +1,73 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s URL\n", os.Args[0])
-		os.Exit(1)
+	var xmlstr string
+	var err error
+
+	if len(os.Args) >= 2 {
+		xmlstr, err = readFromFile(os.Args[1])
+	} else {
+		xmlstr, err = readFromURL("http://bnro.ro/nbrfxrates.xml")
 	}
 
-	response, err := http.Get(os.Args[1])
+	fmt.Println(xmlstr)
 
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		defer response.Body.Close()
-
-		_, err := io.Copy(os.Stdout, response.Body)
-
-		if err != nil {
-			log.Fatal(err)
-		}
 	}
+}
+
+func readFromURL(url string) (string, error) {
+	response, err := http.Get(url)
+
+	if err != nil {
+		return "", err
+	}
+
+	defer response.Body.Close()
+
+	buf, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	xmlstr := fmt.Sprintf("%s", buf)
+
+	return xmlstr, nil
+}
+
+func readFromFile(filename string) (string, error) {
+	f, err := os.Open(filename)
+
+	if err != nil {
+		return "", err
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	var buffer bytes.Buffer
+
+	for scanner.Scan() {
+		buffer.WriteString(scanner.Text())
+		buffer.WriteString("\n")
+	}
+
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	return buffer.String(), nil
 }
