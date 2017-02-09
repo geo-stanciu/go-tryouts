390b9696e75171a1370fd322f9be2453b69250a8
geo <geo.stanciu@hotmail.com>
2017-02-09 23:30:43 +0200

    Query + QeuryRow + sqlparams example

--- go-postgresql/main.go
+++ go-postgresql/main.go
@@ -28,12 +28,53 @@ func main() {
 
 	defer db.Close()
 
+	err = db.Ping()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("opened...")
+
+	now, err := currentTime(db)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	count, err := getStatCount(db)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Date: %s - Stat count: %d\n", now, count)
+}
+
+func getStatCount(db *sql.DB) (int64, error) {
+	var count int64
+
+	query := "select count(*) from pg_stat_activity where usename = $1"
+
+	err := db.QueryRow(query, "geo").Scan(&count)
+
+	switch {
+	case err == sql.ErrNoRows:
+		return 0, err
+	case err != nil:
+		return 0, err
+	default:
+		return count, nil
+	}
+}
+
+func currentTime(db *sql.DB) (string, error) {
+	var currentTime string
 
 	rows, err := db.Query("select current_timestamp")
 
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
 	defer rows.Close()
@@ -43,11 +84,17 @@ func main() {
 		err = rows.Scan(&date)
 
 		if err != nil {
-			log.Fatal(err)
+			return "", err
 		}
 
 		// the format is interpreted by passing date value 2006-01-02 15:04:05.000
 		// in your desired format
-		fmt.Printf("Date: %v\n", date.Format("2006-01-02 15:04:05.000"))
+		currentTime = date.Format("2006-01-02 15:04:05.000")
 	}
+
+	if err := rows.Err(); err != nil {
+		return "", err
+	}
+
+	return currentTime, nil
 }
