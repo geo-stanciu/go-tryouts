6de3519203e78eb6180dbdd768109310157120e6
geo <geo.stanciu@outlook.com>
2017-04-20 23:02:46 +0300

    golang pgdump

---
+++ go-pg-dump/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"time"
+)
+
+type Configuration struct {
+	DumpDir string `json:"DumpDir"`
+	DbName  string `json:"DbName"`
+}
+
+var (
+	config = Configuration{}
+)
+
+func main() {
+	var err error
+	t := time.Now()
+	sData := t.Format("20060102")
+
+	logFile, err := os.OpenFile(fmt.Sprintf("logs/backup_%s.txt", sData), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+
+	mw := io.MultiWriter(os.Stdout, logFile)
+
+	log.SetOutput(mw)
+
+	cfgFile := "./conf.json"
+	err = config.readFromFile(cfgFile)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	dumpFile := path.Join(config.DumpDir, fmt.Sprintf("save_devel_%s.dmp", sData))
+
+	log.Printf("start dump backup \"%s\"\n", dumpFile)
+
+	dump, err := os.OpenFile(dumpFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dump.Close()
+
+	cmd := exec.Command("pg_dump", config.DbName)
+	cmd.Stdout = dump
+
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("end dump backup")
+}
+
+func (c *Configuration) readFromFile(cfgFile string) error {
+	if _, err := os.Stat(cfgFile); os.IsNotExist(err) {
+		return err
+	}
+
+	file, err := os.Open(cfgFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+
+	err = decoder.Decode(&c)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
