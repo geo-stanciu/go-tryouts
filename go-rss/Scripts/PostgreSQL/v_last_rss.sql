1aa72be55159a30096de378f19b84e72831e27d8
geo <geo.stanciu@outlook.com>
2018-03-24 23:15:13 +0200

    Last 100 RSS - view

---
+++ go-rss/Scripts/PostgreSQL/v_last_rss.sql
@@ -0,0 +1,22 @@
+drop view if exists v_last_rss;
+
+create view v_last_rss as
+select
+	rs.source_name as source,
+	r.title,
+	cast(
+		(
+			r.rss_date at time zone 'UTC'
+		) as timestamp with time zone
+	) as local_time,
+	r.link,
+	r.description
+from
+	rss r
+join rss_source rs on
+	(
+		r.rss_source_id = rs.rss_source_id
+	)
+order by
+	rss_date desc
+limit 100;
