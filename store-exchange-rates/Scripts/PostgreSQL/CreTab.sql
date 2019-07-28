94cc06f9d895e741afc7cf7d5576810253b62cf7
geo <geo.stanciu@outlook.com>
2019-07-28 17:00:23 +0300

    No reference currency id in exchange_rate table

--- store-exchange-rates/Scripts/PostgreSQL/CreTab.sql
+++ store-exchange-rates/Scripts/PostgreSQL/CreTab.sql
@@ -10,16 +10,12 @@ create table if not exists exchange_rate (
     currency_id           int            not null,
     exchange_date         date           not null,       
     rate                  numeric(18, 6) not null,
-    reference_currency_id int            not null,
     constraint exchange_rate_pk primary key (currency_id, exchange_date),
     constraint exchange_rate_currency_fk foreign key (currency_id)
-        references currency (currency_id),
-    constraint exchange_rate_ref_currency_fk foreign key (reference_currency_id)
         references currency (currency_id)
 );
 
 create index if not exists idx_exchange_rate_curr_id on exchange_rate (currency_id);
-create index if not exists idx_exchange_rate_refcurr_id on exchange_rate (reference_currency_id);
 create index if not exists idx_exchange_rate_date on exchange_rate (exchange_date);
 
 create table if not exists audit_log (
